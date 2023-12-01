8faf61792d074d6278e9784e0ef4112e63c92eba
VICTOR STEVEN SANTANA GONZALEZ <victor.santana@davivienda.com>
2023-12-01 14:10:42 -0500

    Primer commit

---
+++ src/main/java/com/example/calculatortest/Calculator.java
@@ -0,0 +1,11 @@
+package com.example.calculatortest;
+
+public class Calculator {
+    public double suma(double num1, double num2){
+        return num1+num2;
+    }
+
+    public double resta(double num1, double num2){
+        return num1-num2;
+    }
+}
---
+++ src/test/java/com/example/calculatortest/CalculatorTestApplicationTests.java
@@ -0,0 +1,51 @@
+package com.example.calculatortest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorTestApplicationTests {
+
+    Calculator calculator = new Calculator();
+    
+    @Test
+    void testSuma(){
+        // Given
+        //Calculator calculator = new Calculator();
+
+        // When
+        double result = calculator.suma(3,5);
+
+        // then
+        assertEquals(8, result);
+        assertNotEquals(3, calculator.suma(4.5,6));
+        
+    }
+
+    @Test
+    void testResta(){
+        assertEquals(4, calculator.resta(10,6));
+    }
+
+    @Test
+
+    void testIguales(){
+        assertTrue(calculator.suma(3, 4)==7);
+        assertNotNull(calculator.suma(3, 4));
+    }
+
+    @ParameterizedTest
+
+    @ValueSource(ints = { 1, 2, 3 })
+
+    void testWithValueSource(int argument) {
+        assertTrue(argument > 0 && argument < 4);
+    }
+}
